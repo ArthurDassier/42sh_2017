a41bdfe5e28aa38ab99baccc397e27458ff0cdbe
Vincent <vincent.lemesle@epitech.eu>
2018-05-21 14:20:20 +0200

    [PATCH] ncurses/lib -> errors valgrinds from key_up/key_down -> patch my_itoa (malloc + 1)

--- srcs/shell/ncurses/recup_line.c
+++ srcs/shell/ncurses/recup_line.c
@@ -56,6 +56,7 @@ static char	*read_loop(const char *prompt, t_history **hist_list)
 	__attribute((unused)) const char *,
 	__attribute((unused)) t_history **);
 
+	memset(line, '\0', size);
 	init_buf_function_tab(buf_function);
 	while (read(0, &buf, 1) != 0) {
 		history_completion(*hist_list, line);
