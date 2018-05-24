95daf08a95d5222b2c4ec183040a7f5415c61076
Vincent <vincent.lemesle@epitech.eu>
2018-05-24 11:56:22 +0200

    [RM] recup_line for test

--- srcs/main.c
+++ srcs/main.c
@@ -99,7 +99,9 @@ int	main(__attribute((unused)) int ac, __attribute((unused)) char **av, char
 		prompt_line = prompt(env_list);
 		free_list(cmd_list, &free_lexer);
 		cmd_list = NULL;
-		s = recup_line(prompt_line, &info->hist_list);
+		write(1, prompt_line, strlen(prompt_line));
+		s = get_next_line(0);
+		//s = recup_line(prompt_line, &info->hist_list);
 		s = inib(s);
 		ctrl_d(s, &info->spec_var_list);
 		if (s != NULL && check_char(s) == SUCCESS
